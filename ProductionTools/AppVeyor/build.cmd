ef649b20e0ab5874420d3cb6ba687066297607ac
Roland-Schneider <Roland-Schneider@users.noreply.github.com>
2016-04-15 21:13:27 +0200

    build.cmd more output

--- ProductionTools/AppVeyor/build.cmd
+++ ProductionTools/AppVeyor/build.cmd
@@ -15,7 +15,8 @@ if ERRORLEVEL 1 goto :EOF
 
 :: Generating github release package
 set ReleaseZip=TfZip-Executable.%APPVEYOR_BUILD_VERSION%.zip
-if %APPVEYOR_REPO_TAG%==true set ReleaseZip=TfZip-Executable.%APPVEYOR_REPO_TAG_NAME%.zip
+if "%APPVEYOR_REPO_TAG%"=="true" set ReleaseZip=TfZip-Executable.%APPVEYOR_REPO_TAG_NAME%.zip
+@echo Artifact=%ReleaseZip%
 
 @pushd TfZip\bin
 7z a ..\..\%ReleaseZip% ^
